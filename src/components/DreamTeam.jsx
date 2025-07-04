20dcc597489b29f1b40520ac1a8d7acc4970ea66
sonakorolek <soniakorolyok@gmail.com>
2025-07-05 01:53:39 +0300

    made an array of pictures

--- src/components/DreamTeam.jsx
+++ src/components/DreamTeam.jsx
@@ -1,27 +1,22 @@
-import friend1 from "../assets/Images/friend1.jpg";
-import friend2 from "../assets/Images/friend2.jpg";
-import friend3 from "../assets/Images/friend3.jpg";
-import friend4 from "../assets/Images/friend4.jpg";
-import friend5 from "../assets/Images/friend5.jpg";
-import friend6 from "../assets/Images/friend6.jpg";
-import friend7 from "../assets/Images/friend7.jpg";
-import friend8 from "../assets/Images/friend8.jpg";
-import friend9 from "../assets/Images/friend9.jpg";
+import React from "react";
+import {dreamTeam} from "../utils/constants.js";
 
 
 const DreamTeam = () => {
     return (
         <section className="float-end row w-50 border rounded-bottom-4 me-0 ms-2">
             <h2 className="text-center">Dream team</h2>
-            <img className="col-sm-4 p-1" src={friend1} alt="Friend"/>
-            <img className="col-sm-4 p-1" src={friend2} alt="Friend"/>
-            <img className="col-sm-4 p-1" src={friend3} alt="Friend"/>
-            <img className="col-sm-4 p-1" src={friend4} alt="Friend"/>
-            <img className="col-sm-4 p-1" src={friend5} alt="Friend"/>
-            <img className="col-sm-4 p-1" src={friend6} alt="Friend"/>
-            <img className="bottomLeft col-sm-4 p-1" src={friend7} alt="Friend"/>
-            <img className="col-sm-4 p-1" src={friend8} alt="Friend"/>
-            <img className="bottomRight col-sm-4 p-1" src={friend9} alt="Friend"/>
+            {dreamTeam.map((img, index) => (
+                <img
+                    key={index}
+                    className={`col-sm-4 p-1 
+                            ${index === 6 ? 'bottomLeft' : index === 8 ? 'bottomRight' : ''
+                    }`}
+                    src={img}
+                    title="Friend"
+                    alt="Friend"
+                />
+            ))}
         </section>
     );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,12 @@
+import friend1 from "../assets/Images/friend1.jpg";
+import friend2 from "../assets/Images/friend2.jpg";
+import friend3 from "../assets/Images/friend3.jpg";
+import friend4 from "../assets/Images/friend4.jpg";
+import friend5 from "../assets/Images/friend5.jpg";
+import friend6 from "../assets/Images/friend6.jpg";
+import friend7 from "../assets/Images/friend7.jpg";
+import friend8 from "../assets/Images/friend8.jpg";
+import friend9 from "../assets/Images/friend9.jpg";
 
 export const navItems = ['Home', 'About me', 'Star Wars', 'Contact'];
 export const openingCrawl = `It is a period of civil war.
@@ -21,3 +30,4 @@ export const openingCrawl = `It is a period of civil war.
                 stolen plans that can save her
                 people and restore
                 freedom to the galaxy....`
+export const dreamTeam = [friend1, friend2, friend3, friend4, friend5, friend6, friend7, friend8, friend9];
