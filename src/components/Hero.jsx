2bd1d02eab159ff53987340d0757b6e238d023d7
sonakorolek <soniakorolyok@gmail.com>
2025-07-05 01:34:48 +0300

    mistake fixed #2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import hero from "../assets/Images/main.jpg";
 import React from "react";
 
 
-const OpeningCrawl = () => {
+const Hero = () => {
     return (
         <section className="float-start w-25 me-3">
             <img className="w-100" src={hero} title="Hero" alt="Hero"/>
@@ -11,4 +11,4 @@ const OpeningCrawl = () => {
 };
 
 
-export default OpeningCrawl;
+export default Hero;
