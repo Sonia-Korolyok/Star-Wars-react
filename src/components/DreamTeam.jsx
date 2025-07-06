41623922b614afd263dc6864b0216efb91d2f8e7
sonakorolek <soniakorolyok@gmail.com>
2025-07-06 18:21:20 +0300

    Edd's version

--- src/components/DreamTeam.jsx
+++ src/components/DreamTeam.jsx
@@ -1,25 +1,13 @@
-import React from "react";
-import {dreamTeam} from "../utils/constants.js";
-
+import {friends} from "../utils/constants.js";
+import Friend from "./Friend.jsx";
 
 const DreamTeam = () => {
     return (
         <section className="float-end row w-50 border rounded-bottom-4 me-0 ms-2">
             <h2 className="text-center">Dream team</h2>
-            {dreamTeam.map((img, index) => (
-                <img
-                    key={index}
-                    className={`col-sm-4 p-1 
-                            ${index === 6 ? 'bottomLeft' : index === 8 ? 'bottomRight' : ''
-                    }`}
-                    src={img}
-                    title="Friend"
-                    alt="Friend"
-                />
-            ))}
+            {friends.map((f, i) => <Friend picture={f} key={i} pos={i + 1} />)}
         </section>
     );
 };
-
 
 export default DreamTeam;
