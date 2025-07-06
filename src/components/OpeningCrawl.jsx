781f622f56c5ce461c2d2575c4fe3f5563036292
sonakorolek <soniakorolyok@gmail.com>
2025-07-06 21:01:47 +0300

    Update OpeningCrawl.jsx

--- src/components/OpeningCrawl.jsx
+++ src/components/OpeningCrawl.jsx
@@ -1,10 +1,33 @@
-import {openingCrawl} from "../utils/constants.js";
+import {useEffect, useState} from "react";
+import {base_url} from "../utils/constants.js";
 
 
 const OpeningCrawl = () => {
-    return (
-        <p className="farGalaxy">{openingCrawl}</p>
-    );
+    const [openingCrawl, setOpeningCrawl] = useState('');
+
+    useEffect(() => {
+        const episode = Math.floor( Math.random() * 6 ) + 1;
+        fetch(`${base_url}/v1/films/${episode}`)
+        .then(res => res.json())
+        .then(data => setOpeningCrawl(data.opening_crawl));
+       // return () => console.log('Opening Crawl was unmounted');
+    }, [])
+
+    if (openingCrawl) {
+        return (
+            <p className="farGalaxy">{openingCrawl}</p>
+        );
+    }else {
+        return (
+            <p className="farGalaxy">
+                <span className ="spinner-border spinner-border-sm"></span>
+                Loading...
+            </p>
+
+        )
+    }
+
+
 };
 
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -30,6 +30,7 @@ export const openingCrawl = `It is a period of civil war.
                 stolen plans that can save her
                 people and restore
                 freedom to the galaxy....`
+export const base_url = 'https://sw-info-api.herokuapp.com'
 export const dreamTeam = [friend1, friend2, friend3, friend4, friend5, friend6, friend7, friend8, friend9];
 export const starWarsInfo = `Star Wars is an American epic space opera media franchise created by George Lucas, which began with the eponymous 1977 film and quickly became a worldwide pop culture phenomenon. The franchise has been expanded into various films and other media, including television series, video games, novels, comic books, theme park attractions, and themed areas, comprising an all-encompassing fictional universe. Star Wars is one of the highest-grossing media franchises of all time.
 
