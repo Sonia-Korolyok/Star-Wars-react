4e2ae4775fd851106b29e5fd0ffa922e0baab9d4
sonakorolek <soniakorolyok@gmail.com>
2025-07-06 19:56:19 +0300

    Nav item functionality

--- src/components/DreamTeam.jsx
+++ src/components/DreamTeam.jsx
@@ -1,11 +1,11 @@
-import {friends} from "../utils/constants.js";
+import {dreamTeam} from "../utils/constants.js";
 import Friend from "./Friend.jsx";
 
 const DreamTeam = () => {
     return (
         <section className="float-end row w-50 border rounded-bottom-4 me-0 ms-2">
             <h2 className="text-center">Dream team</h2>
-            {friends.map((f, i) => <Friend picture={f} key={i} pos={i + 1} />)}
+            {dreamTeam.map((f, i) => <Friend picture={f} key={i} pos={i + 1} />)}
         </section>
     );
 };
---
+++ src/components/StarWars.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {starWarsInfo} from "../utils/constants.js";
+
+const StarWars = () => {
+    return (
+        <div className={'farGalaxy'}>
+            {starWarsInfo}
+        </div>
+    );
+};
+
+export default StarWars;
