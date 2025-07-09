ac7d397515e03a7c46db6e733959d55c29a51ec8
sonakorolek <soniakorolyok@gmail.com>
2025-07-09 14:24:26 +0300

    Added picture in AboutMe.jsx prevented default on footer Button

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {aboutMeItems, base_url} from "../utils/constants.js";
 import Spiner from "./Spiner.jsx";
+import Hero from "./Hero.jsx";
 
 const AboutMe = () => {
     const [aboutMe, setAboutMe] = useState({});
@@ -15,6 +16,7 @@ const AboutMe = () => {
         return (
             <div className="container-fluid d-flex justify-content-center align-items-center p-5 ">
                 <div className="text-center fs-2 m-5 border rounded-4 p-4">
+                    <Hero/>
                     {aboutMeItems.map((key) => (
                         <p key={key}>{key}: {aboutMe[key]}</p>
                     ))}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
 const Footer = () => {
     return (
         <footer className="py-4 rounded-bottom-4 row">
-            <div className="btn btn-danger col-sm-2 offset-sm-2">Send me an <span
+            <div className="btn btn-danger col-sm-2 offset-sm-2"
+                 onClick={() => alert('Not yet implemented!')}>Send me an <span
                 className="text-dark text-uppercase">email</span></div>
         </footer>
     );
