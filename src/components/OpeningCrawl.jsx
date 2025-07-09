b4f3321de4354d786cb646565bb769909620900f
sonakorolek <soniakorolyok@gmail.com>
2025-07-09 19:27:39 +0300

    new lesson and Edd's variation of hw

--- src/components/OpeningCrawl.jsx
+++ src/components/OpeningCrawl.jsx
@@ -4,15 +4,23 @@ import Spiner from "./Spiner.jsx";
 
 
 const OpeningCrawl = () => {
-    const [openingCrawl, setOpeningCrawl] = useState('');
+    const [openingCrawl, setOpeningCrawl] = useState();
 
     useEffect(() => {
-        const episode = Math.floor(Math.random() * 6) + 1;
-        fetch(`${base_url}/v1/films/${episode}`)
-            .then(res => res.json())
-            .then(data => setOpeningCrawl(data.opening_crawl));
-        return () => console.log('Opening Crawl was unmounted');
+        const opening_crawl = sessionStorage.getItem("opening_crawl");
+        if (opening_crawl) {
+            setOpeningCrawl(opening_crawl);
+        } else {
+            const episode = Math.floor(Math.random() * 6) + 1;
+            fetch(`${base_url}/v1/films/${episode}`)
+                .then(res => res.json())
+                .then(data => {
+                    setOpeningCrawl(data.opening_crawl)
+                    sessionStorage.setItem("opening_crawl", data.opening_crawl);
+                });
+        }
     }, [])
+
 
     if (openingCrawl) {
         return (
